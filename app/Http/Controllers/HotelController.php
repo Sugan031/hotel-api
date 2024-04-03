6217b7e8d97e057f5b3f998d969f6e9e849bf1d6
Sugan031 <suganthanv031@gmail.com>
2024-04-03 18:29:58 +0530

    hotel validation

---
+++ app/Http/Controllers/HotelController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Hotel;
+use Illuminate\Http\Request;
+
+class HotelController extends Controller {
+    private $dataFromDb;
+
+    public function __construct(Hotel $dataFromDb) {
+        $this->dataFromDb = $dataFromDb;
+    }
+
+    public function getValuesFromdb() {
+        
+        $credentials = $this->dataFromDb->getValues();
+
+        return response()->json($credentials);
+    }
+
+    public function getValuesFromdbSearched(Request $request){
+        $countryName = $request->input('country_name');
+        $city = $request->input('city');
+        $gridNumber = $request->input('grid_number');
+        $uniqueId = $request->input('unique_id');
+        $hotelName = $request->input('name');
+        $validation = $request->input('validation');
+        $credentials = $this->dataFromDb->getValuesFiltered($countryName,$city,$gridNumber,$uniqueId,$hotelName,$validation);
+        return response()->json($credentials);
+    }
+}
---
+++ app/Models/Hotel.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
+
+class Hotel extends Model {
+    use HasFactory;
+
+    protected $table = "sample_hotel_data";
+    public function getValues() {
+            return self::all();
+    }
+
+    public function getValuesFiltered($countryName=null,$city=null,$gridNumber=null,$uniqueId=null,$hotelName=null,$validation=null){
+
+        $data = ['message' => 'no data found'];
+         $query = self::query();
+         if ($countryName) {
+            $query->where('country_name', $countryName);
+        }
+        if ($city) {
+            $query->where('city', $city);
+        }
+        if ($gridNumber) {
+            $query->where('grid_number', $gridNumber);
+        }
+        if ($uniqueId) {
+            $query->where('unique_id', $uniqueId);
+        }
+
+        if ($hotelName) {
+            $query->where('name',$hotelName);
+        }
+        if($countryName ||$city||$gridNumber||$uniqueId||$hotelName){
+             if($validation){
+                $query->where('validation',$validation);
+            }
+        }
+        if($countryName ||$city||$gridNumber||$uniqueId||$hotelName){
+            $data = $query->paginate();
+        }
+
+        return $data;
+    }
+}
